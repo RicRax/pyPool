fcaabc1f20b4a0b66c2abbd1bb3a2fdd0c86fd45
riccardo <ricraz303@gmail.com>
2023-10-23 17:36:53 +0200

    creating tables

--- flaskr/db.py
+++ flaskr/db.py
@@ -7,6 +7,53 @@ def initDB():
     conn.autocommit = True
     cursor = conn.cursor()
 
+    # Create the users table
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS users (
+        user_id SERIAL PRIMARY KEY
+    )
+    """
+    )
+
+    # Create the polls table
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS polls (
+        poll_id SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        description TEXT,
+        created_at TIMESTAMP,
+        user_id INT REFERENCES users(user_id)
+    )
+    """
+    )
+
+    # Create the choices table
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS choices (
+        choice_id SERIAL PRIMARY KEY,
+        choice_text TEXT NOT NULL,
+        poll_id INT REFERENCES polls(poll_id)
+    )
+    """
+    )
+
+    # Create the responses table
+    cursor.execute(
+        """
+    CREATE TABLE IF NOT EXISTS responses (
+        response_id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(user_id),
+        choice_id INT REFERENCES choices(choice_id),
+        chosen_choices INT[]
+    )
+    """
+    )
+
+    # Commit the changes
+    conn.commit()
     # Close the connection
     cursor.close()
     conn.close()
